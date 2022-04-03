fb4e92b9b09805dd5d75f862433a35c128f1478f
Shannooty <75523274+Shannooty@users.noreply.github.com>
2022-04-04 11:42:59 +1200

    Created weapon and armour classes
    methods need to be completed

---
+++ MonsterFighter/src/items/Armour.java
@@ -0,0 +1,13 @@
+package items;
+
+import monsters.Monster;
+
+public class Armour extends Item {
+
+	@Override
+	public void use(Monster monster) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ MonsterFighter/src/items/Weapon.java
@@ -0,0 +1,24 @@
+package items;
+
+import monsters.Monster;
+
+public class Weapon extends Item {
+	
+	private int attackIncrease;
+	
+	public Weapon(String name, int damage) {
+		super(name);
+		attackIncrease = damage;
+	}
+	
+	public int getDamage() {
+		return attackIncrease;
+	}
+	
+	@Override
+	public void use(Monster monster) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
