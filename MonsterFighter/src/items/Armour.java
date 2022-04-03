8fe663c948db765263eabca2c09e9169cf4ba2fc
Shannooty <75523274+Shannooty@users.noreply.github.com>
2022-04-04 11:47:37 +1200

    Update Armour.java
    Added constructor

--- MonsterFighter/src/items/Armour.java
+++ MonsterFighter/src/items/Armour.java
@@ -7,6 +7,12 @@ public class Armour extends Item {
 	private int maxHealthIncrease;
 	private int damageDecrease;
 	
+	public Armour(String name, int healthIncrease, int damageReduction) {
+		super(name);
+		maxHealthIncrease = healthIncrease;
+		damageDecrease = damageReduction;
+	}
+	
 	public int getHealthIncrease() {
 		return maxHealthIncrease;
 	}
