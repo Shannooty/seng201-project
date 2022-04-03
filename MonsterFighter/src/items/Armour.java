51d8427d789d91012ce6de43ade25e0c88556d8a
Shannooty <75523274+Shannooty@users.noreply.github.com>
2022-04-04 11:43:37 +1200

    Update Armour.java

--- MonsterFighter/src/items/Armour.java
+++ MonsterFighter/src/items/Armour.java
@@ -3,7 +3,18 @@ package items;
 import monsters.Monster;
 
 public class Armour extends Item {
-
+	
+	private int maxHealthIncrease;
+	private int damageDecrease;
+	
+	public int getHealthIncrease() {
+		return maxHealthIncrease;
+	}
+	
+	public int getDamageDecrease() {
+		return damageDecrease;
+	}
+	
 	@Override
 	public void use(Monster monster) {
 		// TODO Auto-generated method stub
