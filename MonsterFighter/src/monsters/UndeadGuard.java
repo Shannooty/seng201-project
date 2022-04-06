5ee36ffd859e5dbb0fd0cd7afc67c3e75cb50944
Shannooty <75523274+Shannooty@users.noreply.github.com>
2022-04-06 20:07:15 +1200

    Update UndeadGuard.java
    small fix

--- MonsterFighter/src/monsters/UndeadGuard.java
+++ MonsterFighter/src/monsters/UndeadGuard.java
@@ -5,7 +5,7 @@ public class UndeadGuard extends Monster {
 	public UndeadGuard() {
 		super("Royal Guard #7", 1200, 100, 15);
 		setHealAmount(400);
-		addArmour(100);
+		addArmorAmount(100);
 	}
 	
 }
