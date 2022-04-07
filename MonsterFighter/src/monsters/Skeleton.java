1b420b6b29ff4f71f8ee89cfd11abba98d3b4f14
Shannooty <75523274+Shannooty@users.noreply.github.com>
2022-04-08 11:10:17 +1200

    Update Skeleton.java
    Added path for img

--- MonsterFighter/src/monsters/Skeleton.java
+++ MonsterFighter/src/monsters/Skeleton.java
@@ -2,8 +2,11 @@ package monsters;
 
 public class Skeleton extends Monster {
 	
+	private static String imgPath = "/images/skeleton.png";
+	
 	public Skeleton() {
 		super("Bare Bones", 600, 350, 35);
+		setImgPath(Skeleton.imgPath);
 		setHealAmount(200);
 	}
 	
