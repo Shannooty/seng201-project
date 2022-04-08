4e66a0240b21436c151a0826fdae35a2a170c40a
Shannooty <75523274+Shannooty@users.noreply.github.com>
2022-04-08 12:50:48 +1200

    Update Shop.java
    Monster and Item generators not working currently

--- MonsterFighter/src/shop/Shop.java
+++ MonsterFighter/src/shop/Shop.java
@@ -1,5 +1,56 @@
 package shop;
 
-public class Shop {
+import java.util.ArrayList;
+import items.Item;
+import monsters.Monster;
 
+public class Shop {
+	
+	private ArrayList<Monster> avalibleMonsters = new ArrayList<Monster>();
+	private ArrayList<Item> avalibleItems = new ArrayList<Item>();
+	private int numMonsters;
+	private int numItems;
+	
+	public Shop(int numMonsters, int numItems) {
+		addMonsters();
+		addItems();
+	}
+	
+	private void addMonsters() {
+		for (int i = 0; i < getNumMonsters(); i++) {
+			Monster newMonster = MonsterGenerator.getMonster();
+			avalibleMonsters.add(newMonster);
+		}
+	}
+	
+	private void addItems() {
+		for (int i = 0; i < getNumItems(); i++) {
+			Item newItem = ItemGenerator.getMonster();
+			avalibleItems.add(newItem);
+		}
+	}
+	
+	public ArrayList<Monster> getAvalibleMonsters(){
+		return avalibleMonsters;
+	}
+	
+	public ArrayList<Item> getAvalibleItems(){
+		return avalibleItems;
+	}
+	
+	public int getNumMonsters() {
+		return numMonsters;
+	}
+
+	public void setNumMonsters(int numMonsters) {
+		this.numMonsters = numMonsters;
+	}
+
+	public int getNumItems() {
+		return numItems;
+	}
+
+	public void setNumItems(int numItems) {
+		this.numItems = numItems;
+	}
 }
