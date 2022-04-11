a2d8c2c4863e7a3312488840079723fc50cd755b
Shannooty <75523274+Shannooty@users.noreply.github.com>
2022-04-11 17:37:12 +1200

    Update AvalibleFoods.java

---
+++ MonsterFighter/src/generators/AvalibleFoods.java
@@ -0,0 +1,13 @@
+package generators;
+
+import java.util.Random;
+
+public enum AvalibleFoods {
+	APPLE;
+	
+	private static final AvalibleFoods[] VALUES = AvalibleFoods.values();
+	
+	public static AvalibleFoods randomFood() {
+		return VALUES[new Random().nextInt(VALUES.length)];
+	}
+}
