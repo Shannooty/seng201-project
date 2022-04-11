c209242fee2aebd3bf05b619e6ba06c38b13f2ba
Shannooty <75523274+Shannooty@users.noreply.github.com>
2022-04-11 17:09:17 +1200

    Update AvalibleMonsters.java
    Random monster types can be accessed

--- MonsterFighter/src/generators/AvalibleMonsters.java
+++ MonsterFighter/src/generators/AvalibleMonsters.java
@@ -1,5 +1,13 @@
 package generators;
 
+import java.util.Random;
+
 public enum AvalibleMonsters {
-	SLIME, ZOMBIE, UNDEADGUARD, SKELETON
+	SLIME, ZOMBIE, UNDEADGUARD, SKELETON;
+	
+	private static final AvalibleMonsters[] VALUES = AvalibleMonsters.values();
+	
+	public static AvalibleMonsters randomMonster() {
+		return VALUES[new Random().nextInt(VALUES.length)];
+	}
 }
