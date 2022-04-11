34ef4841802cf0cd6a08d87440495160b622cf9a
Shannooty <75523274+Shannooty@users.noreply.github.com>
2022-04-11 18:17:45 +1200

    Day.java is no longer broken

--- MonsterFighter/src/day/Day.java
+++ MonsterFighter/src/day/Day.java
@@ -1,15 +1,31 @@
 package day;
 import java.util.ArrayList;
 
+import shop.Shop;
+
 public class Day {
 	
-	private int dayNumber = 1;
+	private int dayNumber;
 	private Shop todaysShop;
-	private ArrayList<Battle> todaysBattles = new ArrayList<Battle>;
+	private ArrayList<Battle> todaysBattles = new ArrayList<Battle>();
 	
+	public Day(int dayNumber) {
+		setDayNumber(dayNumber);
+		todaysShop = new Shop(3, 5); //parameters could be randomized
+		createDailyBattles(5);
+	}
+	
+	private void setDayNumber(int dayNum) {
+		dayNumber = dayNum;
+	}
+	
+	private void createDailyBattles(int numOfBattles) {
+		for (int i = 0; i < numOfBattles; i++) {
+			todaysBattles.add(new Battle());
+		}
+	}
 	
 	public void viewShop() {
-//		create instance of Shop
 //		open shop window
 //		testing
 //		testing again
@@ -18,15 +34,13 @@ public class Day {
 	
 	public void sleep() {
 //		create instance of RandomEvent
-		increaseDayNum();
+		
 	}
 	
 	public void battle() {
 //		create instance of Battle
 	}
 	
-	public void increaseDayNum() {
-		dayNumber += 1;
-	}
+	
 
 }
