54b6f26f86c7b42ebf8ef01ae9d8a62fc7114d37
Shannooty <75523274+Shannooty@users.noreply.github.com>
2022-04-11 17:53:44 +1200

    Create ItemGenerator.java
    Methods are static so can be called from anywhere to give a random Item

---
+++ MonsterFighter/src/generators/ItemGenerator.java
@@ -0,0 +1,66 @@
+package generators;
+
+import purchasable.items.*;
+import purchasable.items.armors.*;
+import purchasable.items.weapons.*;
+import purchasable.items.food.*;
+
+public class ItemGenerator extends Generator {
+	
+	public static Item newItem() {
+		AvalibleItemTypes itemType = AvalibleItemTypes.randomItemType();
+		
+		switch (itemType) {
+		case ARMOR:
+			return newArmor();
+		case FOOD:
+			return newFood();
+		case POTION:
+			return newPotion();
+		case WEAPON:
+			return newWeapon();
+		default:
+			return null;
+		}
+	}
+	
+	public static Armor newArmor() {
+		AvalibleArmors armorType = AvalibleArmors.randomArmor();
+		
+		switch(armorType) {
+		case FLOWERCROWN:
+			return new FlowerCrown();
+		default:
+			return null;
+		
+		}
+	}
+	
+	public static Food newFood() {
+		AvalibleFoods foodType = AvalibleFoods.randomFood();
+		
+		switch(foodType) {
+		case APPLE:
+			return new Apple();
+		default:
+			return null;
+		
+		}
+	}
+	
+	public static Weapon newWeapon() {
+		AvalibleWeapons weaponType = AvalibleWeapons.randomWeapon();
+		
+		switch(weaponType) {
+		case SHARPSTICK:
+			return new SharpStick();
+		default:
+			return null;
+		
+		}
+	}
+	
+	public static SpeedPotion newPotion() {
+		return new SpeedPotion();
+	}
+}
