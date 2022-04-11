e3aece24924feca52b3f21a782a619ca8fcf7abd
Shannooty <75523274+Shannooty@users.noreply.github.com>
2022-04-11 17:13:10 +1200

    MonsterGenerator newMonster method
    Can be called staticly to create a new random monster

--- MonsterFighter/src/generators/MonsterGenerator.java
+++ MonsterFighter/src/generators/MonsterGenerator.java
@@ -1,10 +1,23 @@
 package generators;
 
-import purchasable.monsters.Monster;
+import purchasable.monsters.*;
 
 public class MonsterGenerator extends Generator {
 	
-	public Monster newMonster() {
+	public static Monster newMonster() {
+		AvalibleMonsters monsterType = AvalibleMonsters.randomMonster();
 		
+		switch (monsterType) {
+		case SKELETON:
+			return new Skeleton();
+		case SLIME:
+			return new Slime();
+		case UNDEADGUARD:
+			return new UndeadGuard();
+		case ZOMBIE:
+			return new Zombie();
+		default:
+			return null;
+		}
 	}
 }
