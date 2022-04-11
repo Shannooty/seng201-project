5ba7232617b22cdcc5d9d71b4d3383514f4e2a98
Shannooty <75523274+Shannooty@users.noreply.github.com>
2022-04-11 16:57:50 +1200

    Purchasable.java base methods and variables

--- MonsterFighter/src/purchasable/Purchasable.java
+++ MonsterFighter/src/purchasable/Purchasable.java
@@ -1,5 +1,40 @@
 package purchasable;
 
 public class Purchasable {
-
+	private double purchasePrice;
+	private double sellPrice;
+	private String discription;
+	
+	public void buy() {
+		
+	}
+	
+	public void sell() {
+		
+	}
+	
+	public double getPurchasePrice() {
+		return purchasePrice;
+	}
+	
+	public double getSellPrice() {
+		return sellPrice;
+	}
+	
+	public String getDiscription() {
+		return discription;
+	}
+	
+	public void setDiscription(String discription) {
+		this.discription = discription;
+	}
+	
+	public void setPurchasePrice(double buyPrice) {
+		purchasePrice = buyPrice;
+	}
+	
+	public void setSellPrice(double sellPrice) {
+		this.sellPrice = sellPrice;
+	}
+	
 }
