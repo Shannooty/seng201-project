87b3debf694c6d4ad051507d82006739435e7628
Chamille9 <cal135@uclive.ac.nz>
2022-04-13 11:12:32 +1200

    Javadoc started, unsure on description of some methods. Not done.

--- MonsterFighter/src/random_event/NewMonsterJoins.java
+++ MonsterFighter/src/random_event/NewMonsterJoins.java
@@ -6,18 +6,30 @@ import purchasable.monsters.*;
 
 
 
-
+/**
+ * 
+ * @author 
+ *
+ */
 public class NewMonsterJoins extends RandomEvent {
 
-	
+	/**
+	 * Attribute random, of type Random. A random generator.
+	 */
 	Random random = new Random();
 	
-	
+	/**
+	 * Constructor for class NewMonsterJoins. ______???? and calls the method createMonster().
+	 */
 	public NewMonsterJoins() {	
 		AvalibleMonsters[] monsterList = AvalibleMonsters.values();
 		createMonster(monsterList[random.nextInt(monsterList.length)]);
 	}
 	
+	/**
+	 * Creates a new Monster by calling the getNewMonster() method, and adds it to the player's team.
+	 * @param monsterType, of type AvailableMonsters. A list(????) of available monsters.
+	 */
 	public void createMonster(AvalibleMonsters monsterType) {
 		
 		Monster newMonster;
@@ -26,6 +38,11 @@ public class NewMonsterJoins extends RandomEvent {
 		
 	}
 	
+	/**
+	 * Creates a new Monster based on the value of the variable monsterType passed to it.
+	 * @param monsterType, of type AvailableMonsters. A list(????) of available monsters.
+	 * @return a new Monster, or null.
+	 */
 	public Monster getNewMonster(AvalibleMonsters monsterType) {
 		switch(monsterType) {
 		case SLIME:
@@ -40,6 +57,10 @@ public class NewMonsterJoins extends RandomEvent {
 		return null;
 	}
 
+	/**
+	 * Adds the given Monster to the player's Inventory. Return type void.
+	 * @param monster, of type Monster. The Monster to be added to the player's Inventory.
+	 */
 	public void addMonster(Monster monster) {
 		getPlayerTeam().add(monster);
 	}
