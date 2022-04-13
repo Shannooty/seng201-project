cd9ab86f301e2a074c035276e6e84a5ba1b59d98
Shannooty <75523274+Shannooty@users.noreply.github.com>
2022-04-13 13:53:37 +1200

    Inventory button for Items

---
+++ MonsterFighter/src/gui/customElements/InventoryToggleButton.java
@@ -0,0 +1,38 @@
+package gui.customElements;
+
+import javax.swing.JToggleButton;
+
+import purchasable.Purchasable;
+import purchasable.items.Item;
+import purchasable.monsters.Monster;
+
+public class InventoryToggleButton extends JToggleButton {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5449563501811900529L;
+	
+	private Purchasable buttonObject;
+	
+	
+	public InventoryToggleButton(Monster item) {
+		setButtonObject(item);
+		this.setIcon(item.getImg());
+		this.setBorder(null);
+		this.setSelectedIcon(item.getImg());
+	}
+	
+	public InventoryToggleButton(Item item) {
+		setButtonObject(item);
+		this.setIcon(item.getImg());
+	}
+
+	public Purchasable getButtonObject() {
+		return buttonObject;
+	}
+
+	private void setButtonObject(Purchasable buttonObject) {
+		this.buttonObject = buttonObject;
+	}
+}
