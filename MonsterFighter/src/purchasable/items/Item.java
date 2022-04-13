b11a8d31a0098fcf1b5a28c858c5912f74a8000e
Shannooty <75523274+Shannooty@users.noreply.github.com>
2022-04-13 14:44:23 +1200

    Moved name up to Purchasable.java

--- MonsterFighter/src/purchasable/items/Item.java
+++ MonsterFighter/src/purchasable/items/Item.java
@@ -1,31 +1,20 @@
 package purchasable.items;
 
-import javax.swing.Icon;
-import javax.swing.ImageIcon;
-
 import purchasable.Purchasable;
 import purchasable.monsters.*;
 
 public abstract class Item extends Purchasable {
-	private String itemName;
 	
 	public Item(String name) {
-		setItemName(name);
+		super(name);
 	}
 	
-	public void setItemName(String name) {
-		itemName = name;
-	}
-	
-	public String getItemName() {
-		return itemName;
-	}
 	
 	public abstract Item use(Monster monster);
 	
 	
 	public String toString() {
-		return "Type: "+ getClass()+"\nName: " + getItemName()+"\n\nPrice: "+getPurchasePrice();
+		return "Type: "+ getClass()+"\nName: " + getName()+"\n\nPrice: "+getPurchasePrice();
 	}
 
 }
