1c5807762bb48d4abd18a3648a824322a0cf1422
Shannooty <75523274+Shannooty@users.noreply.github.com>
2022-04-13 14:06:46 +1200

    Create InventoryButtonGroup.java

---
+++ MonsterFighter/src/gui/customElements/InventoryButtonGroup.java
@@ -0,0 +1,16 @@
+package gui.customElements;
+
+import javax.swing.ButtonGroup;
+
+public class InventoryButtonGroup extends ButtonGroup {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1370573140275999672L;
+	
+	public InventoryButtonGroup() {
+		super();
+	}
+
+}
