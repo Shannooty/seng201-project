3cfc7c76cffa03a247a0c30125372e2e69e94c22
Shannooty <75523274+Shannooty@users.noreply.github.com>
2022-04-15 18:20:02 +1200

    Leaderboard.java methods done

--- MonsterFighter/src/leaderboard/Leaderboard.java
+++ MonsterFighter/src/leaderboard/Leaderboard.java
@@ -2,6 +2,7 @@ package leaderboard;
 
 import java.io.*;
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.TreeSet;
 
 import player.PlayerScore;
@@ -10,11 +11,37 @@ public class Leaderboard {
 	
 	private TreeSet<PlayerScore> leaderboard = new TreeSet<PlayerScore>();
 	private String leaderboardFilePath = "leaderboard.txt";
+	private String lineFormat = "%s:%s\n";
 	
 	public Leaderboard() {
 		load();
 	}
 
+	
+	public TreeSet<PlayerScore> getLeaderboard() {
+		return leaderboard;
+	}
+	
+	public void addScore(PlayerScore score) {
+		getLeaderboard().add(score);
+		save();
+	}
+	
+	public int getPosition(PlayerScore score) {
+		Iterator<PlayerScore> leaderboardIterator = getLeaderboard().iterator();
+		int playerPosition = -1;
+		int counter = 0;
+		
+		while (playerPosition == -1 && leaderboardIterator.hasNext()) {
+			counter++;
+			if (leaderboardIterator.next() == score) {
+				playerPosition = counter;
+			}
+		}
+		
+		return playerPosition;
+	}
+	
 	private void load() {
 		try {
 			BufferedReader br = new BufferedReader(
@@ -29,12 +56,31 @@ public class Leaderboard {
 				PlayerScore playerScore = new PlayerScore(playerName, playerPoints);
 				getLeaderboard().add(playerScore);
 			}
+			
+			br.close();
 		} catch (IOException e) {
 			return;
 		}
 	}
 	
-	public TreeSet<PlayerScore> getLeaderboard() {
-		return leaderboard;
+	private void save() {
+		try {
+			BufferedWriter bw = new BufferedWriter(
+					new FileWriter(leaderboardFilePath));
+			
+			for (PlayerScore score : getLeaderboard()) {
+				String fileLine = String.format(lineFormat, score.getName(), score.getPoints());
+				System.out.println(fileLine);
+				bw.write(fileLine);
+			}
+			
+			bw.close();
+		} catch (IOException e) {
+			return;
+		}
+	}
+	
+	public static void main(String[] args) {
+		
 	}
 }
