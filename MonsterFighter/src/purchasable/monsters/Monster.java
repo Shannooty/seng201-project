0ac7c377b9b899a85e4d7145a2fe5d5510e6d7f2
Chamille9 <cal135@uclive.ac.nz>
2022-04-15 13:08:34 +1200

    Style changes in monster toString

--- MonsterFighter/src/purchasable/monsters/Monster.java
+++ MonsterFighter/src/purchasable/monsters/Monster.java
@@ -207,13 +207,13 @@ public abstract class Monster extends Purchasable implements Comparator<Monster>
 	public String toString() {
 		String type = getMonsterType().replaceAll("([A-Z])", " $1");
 		type = type.substring(0, 1).toUpperCase() + type.substring(1);
-		return "Type: "+type+"\nName: "+getName()+"\nHealth: "+getHealth()+"\nAttack Amount: "+getAttackAmount()+"\nSpeed: "+getSpeed()+"\n\nPrice: "+getPurchasePrice();
+		return "Type: "+type+"\nName: "+getName()+"\n\nHealth: "+getHealth()+"\nAttack Amount: "+getAttackAmount()+"\nSpeed: "+getSpeed()+"\n\nPrice: "+getPurchasePrice();
 	}
 	
 	public String getDescription() {
 		String type = getMonsterType().replaceAll("([A-Z])", " $1");
 		type = type.substring(0, 1).toUpperCase() + type.substring(1);
-		return type+": "+getName()+" "+getID()+"gg";
+		return type+": "+getName()+" "+getID();
 	}
 	
 	@Override
