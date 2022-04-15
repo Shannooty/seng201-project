e2b64e9bbd5d4c02200798f0236eed43adf6a170
Shannooty <75523274+Shannooty@users.noreply.github.com>
2022-04-15 17:59:34 +1200

    Leaderboard no longer broken

--- MonsterFighter/src/leaderboard/Leaderboard.java
+++ MonsterFighter/src/leaderboard/Leaderboard.java
@@ -1,5 +1,40 @@
 package leaderboard;
 
-public class Leaderboard {
+import java.io.*;
+import java.io.IOException;
+import java.util.TreeSet;
 
+import player.PlayerScore;
+
+public class Leaderboard {
+	
+	private TreeSet<PlayerScore> leaderboard = new TreeSet<PlayerScore>();
+	private String leaderboardFilePath = "leaderboard.txt";
+	
+	public Leaderboard() {
+		load();
+	}
+
+	private void load() {
+		try {
+			BufferedReader br = new BufferedReader(
+					new FileReader(leaderboardFilePath));
+			String line;
+			String playerName;
+			String playerPoints;
+			
+			while((line = br.readLine()) != null) {
+				playerName = line.split(":")[0];
+				playerPoints = line.split(":")[1];
+				PlayerScore playerScore = new PlayerScore(playerName, playerPoints);
+				getLeaderboard().add(playerScore);
+			}
+		} catch (IOException e) {
+			return;
+		}
+	}
+	
+	public TreeSet<PlayerScore> getLeaderboard() {
+		return leaderboard;
+	}
 }
