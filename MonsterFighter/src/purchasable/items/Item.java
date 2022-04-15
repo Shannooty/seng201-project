fe5262cb044276345227feef0b96fbd4c7fd0068
Chamille9 <cal135@uclive.ac.nz>
2022-04-15 13:37:07 +1200

    Added item description to shop

--- MonsterFighter/src/purchasable/items/Item.java
+++ MonsterFighter/src/purchasable/items/Item.java
@@ -2,6 +2,9 @@ package purchasable.items;
 
 import player.Player;
 import purchasable.Purchasable;
+import purchasable.items.armors.Armor;
+import purchasable.items.food.Food;
+import purchasable.items.weapons.Weapon;
 import purchasable.monsters.*;
 
 public abstract class Item extends Purchasable {
@@ -26,7 +29,24 @@ public abstract class Item extends Purchasable {
 	
 	
 	public String toString() {
-		return "Type: "+ getClass()+"\nName: " + getName()+"\n\nPrice: "+getPurchasePrice();
+		String type = (String.valueOf(getClass()).split("\\."))[2];
+		String typeEdited = (type.substring(0, 1).toUpperCase() + type.substring(1, type.length())).replaceAll("s", "");
+		String effect = "";
+		
+		if (this instanceof Food) {
+			effect = "Heal: " + Integer.toString(((Food) this).getHealAmount());
+		} else if (this instanceof Weapon) {
+			effect = "Increase attack damage: " +Integer.toString(((Weapon) this).getDamage());
+		} else if (this instanceof Armor) {
+			effect = "Increase armor: "+Integer.toString(((Armor) this).getArmorIncrease());
+			effect += "\nIncrease maximum health: "+Integer.toString(((Armor) this).getHealthIncrease());
+		} else if (this instanceof SpeedPotion) {
+			effect = "Increase speed: "+Integer.toString(((SpeedPotion) this).getSpeedIncrease());
+		}
+			
+
+		
+		return "Type: "+typeEdited+"\nName: " + getName()+"\n\n"+effect+"\n\nPrice: "+getPurchasePrice();
 	}
 
 }
