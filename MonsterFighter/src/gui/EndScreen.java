42a977a1e7409076febc5099cdf2edf7c49dc93d
Shannooty <75523274+Shannooty@users.noreply.github.com>
2022-04-15 19:37:02 +1200

    Update EndScreen.java
    UI elements inplace

--- MonsterFighter/src/gui/EndScreen.java
+++ MonsterFighter/src/gui/EndScreen.java
@@ -4,31 +4,26 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 
+import player.Player;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JList;
+import javax.swing.JButton;
+
 public class EndScreen {
 
 	private JFrame frame;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					EndScreen window = new EndScreen();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
+	private GameEnvironment gameEnvironment;
+	private Player player;
+	
 	/**
 	 * Create the application.
 	 */
-	public EndScreen() {
+	public EndScreen(GameEnvironment manager) {
 		initialize();
+		gameEnvironment = manager;
+		setPlayer(manager.getPlayer());
 	}
 
 	/**
@@ -36,8 +31,52 @@ public class EndScreen {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 850, 570);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblGameOver = new JLabel("Game Over!");
+		lblGameOver.setHorizontalAlignment(SwingConstants.CENTER);
+		lblGameOver.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		lblGameOver.setBounds(248, 11, 337, 53);
+		frame.getContentPane().add(lblGameOver);
+		
+		JLabel lblScore = new JLabel("Score : #####");
+		lblScore.setHorizontalAlignment(SwingConstants.CENTER);
+		lblScore.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblScore.setBounds(257, 61, 320, 53);
+		frame.getContentPane().add(lblScore);
+		
+		JLabel lblLeaderboard = new JLabel("Leaderboard");
+		lblLeaderboard.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblLeaderboard.setHorizontalAlignment(SwingConstants.CENTER);
+		lblLeaderboard.setBounds(295, 115, 244, 46);
+		frame.getContentPane().add(lblLeaderboard);
+		
+		JList listLeaderboard = new JList();
+		listLeaderboard.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		listLeaderboard.setVisibleRowCount(20);
+		listLeaderboard.setBounds(295, 157, 244, 306);
+		frame.getContentPane().add(listLeaderboard);
+		
+		JButton btnPlayAgain = new JButton("Play Again");
+		btnPlayAgain.setBounds(295, 478, 110, 28);
+		frame.getContentPane().add(btnPlayAgain);
+		
+		JButton btnQuit = new JButton("Quit");
+		btnQuit.setBounds(429, 478, 110, 28);
+		frame.getContentPane().add(btnQuit);
 	}
 
+	public GameEnvironment getGameEnvironment() {
+		return gameEnvironment;
+	}
+
+	public Player getPlayer() {
+		return player;
+	}
+
+	public void setPlayer(Player player) {
+		this.player = player;
+	}
 }
