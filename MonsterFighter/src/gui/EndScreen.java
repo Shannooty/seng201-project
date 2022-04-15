eade44cb8bbfa45e77302715429391f101372be7
Shannooty <75523274+Shannooty@users.noreply.github.com>
2022-04-15 19:50:21 +1200

    Update EndScreen.java

--- MonsterFighter/src/gui/EndScreen.java
+++ MonsterFighter/src/gui/EndScreen.java
@@ -5,17 +5,24 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 
 import player.Player;
+import player.PlayerScore;
+
 import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.SwingConstants;
+
+import leaderboard.Leaderboard;
+
 import javax.swing.JList;
 import javax.swing.JButton;
+import javax.swing.ListSelectionModel;
 
 public class EndScreen {
 
 	private JFrame frame;
 	private GameEnvironment gameEnvironment;
 	private Player player;
+	private Leaderboard leaderboard;
 	
 	/**
 	 * Create the application.
@@ -24,6 +31,26 @@ public class EndScreen {
 		initialize();
 		gameEnvironment = manager;
 		setPlayer(manager.getPlayer());
+		setLeaderboard(GameEnvironment.getLeaderboard());
+	}
+	
+	public void closeWindow() {
+		frame.dispose();
+	}
+	
+	
+	public void finishedWindow() {
+		gameEnvironment.closeEndScreen(this);
+	}
+	
+	private void loadLeaderboard(JList list) {
+		int position = 0;
+		for (PlayerScore score : getLeaderboard().getLeaderboard()) {
+			String displayMessage = position++ + " " + score;
+			JLabel entry = new JLabel(displayMessage);
+			list.add(entry);
+		}
+		
 	}
 
 	/**
@@ -54,10 +81,13 @@ public class EndScreen {
 		frame.getContentPane().add(lblLeaderboard);
 		
 		JList listLeaderboard = new JList();
+		listLeaderboard.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		listLeaderboard.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		listLeaderboard.setVisibleRowCount(20);
 		listLeaderboard.setBounds(295, 157, 244, 306);
 		frame.getContentPane().add(listLeaderboard);
+		
+		loadLeaderboard(listLeaderboard);
 		
 		JButton btnPlayAgain = new JButton("Play Again");
 		btnPlayAgain.setBounds(295, 478, 110, 28);
@@ -78,5 +108,13 @@ public class EndScreen {
 
 	public void setPlayer(Player player) {
 		this.player = player;
+	}
+
+	public Leaderboard getLeaderboard() {
+		return leaderboard;
+	}
+
+	public void setLeaderboard(Leaderboard leaderboard) {
+		this.leaderboard = leaderboard;
 	}
 }
