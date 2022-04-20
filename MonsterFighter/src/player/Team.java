30f8e47d2264bb0469f35a959a2ca51cf30adfcc
Shannooty <75523274+Shannooty@users.noreply.github.com>
2022-04-20 18:56:52 +1200

    Team.java javadoc

--- MonsterFighter/src/player/Team.java
+++ MonsterFighter/src/player/Team.java
@@ -5,12 +5,27 @@ import java.util.ArrayList;
 import exceptions.TeamFullException;
 import purchasable.monsters.Monster;
 
+/**
+ * Class that implements the Team functionality for the Player and sorts the Team by their speed attribute
+ * @author Bede Nathan, Celia Allen
+ *
+ */
 public class Team {
 	public ArrayList<Monster> team = new ArrayList<Monster>();
 	
+	
+	/**
+	 * To access the Team Monster array
+	 * @return ArrayList of the Team Monsters
+	 */
 	public ArrayList<Monster> getTeam() {
 		return team;
 	}
+	
+	/**
+	 * Adds a monster to the Team unless the Team is full, in which case will throw a TeamFullException
+	 * @param monster Monster to be added
+	 */
 	
 	public void add(Monster monster) {
 		if (monster != null) {
@@ -25,20 +40,35 @@ public class Team {
 		}
 	}
 	
+	/**
+	 * Removes a specific Monster from the Team
+	 * @param monster Monster to be removed
+	 */
 	public void remove(Monster monster) {
 		getTeam().remove(monster);
 		refreshOrder();
 	}
 	
+	/**
+	 * Refreshes the team order when adding or removing Monsters
+	 */
 	public void refreshOrder() {
 		sort(getTeam());
 	}
 	
+	/**
+	 * Gives the size of the team
+	 * @return Size of team
+	 */
 	public int size() {
 		return getTeam().size();
 	}
 	
-	public void sort(ArrayList<Monster> list)
+	/**
+	 * Does the sorting for the team
+	 * @param list
+	 */
+	private void sort(ArrayList<Monster> list)
     {
         list.sort((o1, o2) -> Integer.compare(o1.getSpeed(), o2.getSpeed()));
     }
