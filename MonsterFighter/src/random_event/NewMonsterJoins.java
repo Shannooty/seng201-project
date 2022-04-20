fac3f1f9f4ed4dbd16c30d7a8acf3a3d2b0f74f9
Shannooty <75523274+Shannooty@users.noreply.github.com>
2022-04-20 17:59:31 +1200

    NewMonsterJoins bug fixes and cleaned up code

--- MonsterFighter/src/random_event/NewMonsterJoins.java
+++ MonsterFighter/src/random_event/NewMonsterJoins.java
@@ -15,49 +15,28 @@ import purchasable.monsters.*;
  */
 public class NewMonsterJoins extends RandomEvent {
 
-	/**
-	 * Attribute random, of type Random. A random generator.
-	 */
-	Random random = new Random();
 	
 	/**
 	 * Constructor for class NewMonsterJoins. ______???? and calls the method createMonster().
 	 */
 	public NewMonsterJoins(Inventory inventory) {
 		super(inventory);
-		AvalibleMonsters[] monsterList = AvalibleMonsters.values();
-		createMonster(monsterList[random.nextInt(monsterList.length)]);
+		
+		if (getPlayerTeam().size() < 4) {
+			createMonster();
+		}
 	}
 	
 	/**
 	 * Creates a new Monster by calling the getNewMonster() method, and adds it to the player's team.
 	 * @param monsterType, of type AvailableMonsters. A list(????) of available monsters.
 	 */
-	public void createMonster(AvalibleMonsters monsterType) {
+	public void createMonster() {
 		
 		Monster newMonster;
 		newMonster = MonsterGenerator.newMonster();
 		addMonster(newMonster);
 		
-	}
-	
-	/**
-	 * Creates a new Monster based on the value of the variable monsterType passed to it.
-	 * @param monsterType, of type AvailableMonsters. A list(????) of available monsters.
-	 * @return a new Monster, or null.
-	 */
-	public Monster getNewMonster(AvalibleMonsters monsterType) {
-		switch(monsterType) {
-		case SLIME:
-			return new Slime();
-		case SKELETON:
-			return new Skeleton();
-		case UNDEADGUARD:
-			return new UndeadGuard();
-		case ZOMBIE:
-			return new Zombie();
-		}
-		return null;
 	}
 
 	/**
