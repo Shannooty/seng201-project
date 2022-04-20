fc58ce2530dc5fb7835b549ef4dd26148fd79de7
Shannooty <75523274+Shannooty@users.noreply.github.com>
2022-04-20 17:56:59 +1200

    Create RandomEventTest.java
    Added tests for MonsterJoins

---
+++ MonsterFighter/src/test/RandomEventTest.java
@@ -0,0 +1,73 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import generators.MonsterGenerator;
+import player.Player;
+import player.Team;
+import purchasable.monsters.Monster;
+import random_event.*;
+
+class RandomEventTest {
+	
+	private RandomEvent testEvent;
+	private Player testPlayer;
+	private Team playerTeam;
+	private Monster startingMonster = MonsterGenerator.newMonster();
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		testPlayer = new Player("Bob", startingMonster);
+		playerTeam = testPlayer.getInventory().getTeam();
+		testEvent = new RandomEvent(testPlayer.getInventory());
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		testPlayer = null;
+		playerTeam = null;
+		testEvent = null;
+	}
+
+	@Test
+	void testGetRandomEvent() {
+		ArrayList<String> validEvents = new ArrayList<String>();
+		validEvents.add("MonsterLeaves");
+		validEvents.add("NewMonsterJoins");
+		validEvents.add("MonsterLevelsUp");
+		
+		String outputEvent;
+		
+		//Run test 15 times to get all outputs
+		for (int i = 0; i < 15; i++) {
+			outputEvent = testEvent.getRandomEvent();
+			assertTrue(validEvents.contains(outputEvent));
+		}
+		
+	}
+	
+	@Test
+	void runNewMonsterJoins() {
+		assertEquals(1, playerTeam.size());
+		
+		NewMonsterJoins monsterJoinsEvent;
+		
+		//Should fill up the team with new monsters
+		for (int i = 2; i < 5; i++) {
+			monsterJoinsEvent = new NewMonsterJoins(testPlayer.getInventory());
+			assertEquals(i, playerTeam.size());
+		}
+		
+		//One more NewMonsterJoins shouldn't do anything
+		monsterJoinsEvent = new NewMonsterJoins(testPlayer.getInventory());
+		assertEquals(4, playerTeam.size());
+		
+	}
+
+}
