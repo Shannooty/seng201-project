46b5409170f6127d3ae2b9a469e5b5ef4ccf5cc4
Shannooty <75523274+Shannooty@users.noreply.github.com>
2022-04-22 00:51:00 +1200

    Update InventoryTest.java
    Added tests

--- MonsterFighter/src/test/InventoryTest.java
+++ MonsterFighter/src/test/InventoryTest.java
@@ -25,7 +25,7 @@ class InventoryTest {
 	
 	private void fillTestArrays() {
 		testItems.add(new Apple());
-		testItems.add(new FlowerCrown());
+		testItems.add(new Chainmail());
 		testItems.add(new SpeedPotion());
 		testItems.add(new SharpStick());
 		
@@ -85,7 +85,7 @@ class InventoryTest {
 		testInventory.removeItem(testItems.get(3));
 		assertEquals(0, inventoryItems.size());
 		
-		//Remove an item from emtpy inventory
+		//Remove an item from empty inventory
 		testInventory.removeItem(testItems.get(0));
 		assertEquals(0, inventoryItems.size());
 		
@@ -130,5 +130,54 @@ class InventoryTest {
 		//Remove the one object
 		testInventory.removeMonster(inventoryMonsters.get(0));
 		assertEquals(0, inventoryMonsters.size());
+	}
+	
+	@Test
+	void testUseItem() {
+		Monster playerMonster = testInventory.getTeam().getTeam().get(0);
+		playerMonster.removeHealth(200);
+		int monsterHealth = playerMonster.getHealth();
+		int monsterSpeed = playerMonster.getSpeed();
+		int monsterMaxHealth = playerMonster.getMaxHealth();
+		int monsterAttackDamage = playerMonster.getAttackAmount();
+		int monsterArmorAmount = playerMonster.getArmorAmount();
+		
+		//Give inventory a food, speedpotion, weapon and armor
+		testInventory.addItem(testItems.get(0));
+		testInventory.addItem(testItems.get(1));
+		testInventory.addItem(testItems.get(2));
+		testInventory.addItem(testItems.get(3));
+		
+		//Food test
+		testInventory.useItem(testInventory.getItems().get(0), playerMonster);
+		assertEquals(3, testInventory.getItems().size());
+		assertFalse(monsterHealth == playerMonster.getHealth());
+		
+		monsterHealth = playerMonster.getHealth();
+		
+		//Armor test
+		testInventory.useItem(testInventory.getItems().get(0), playerMonster);
+		assertEquals(0, testInventory.getItems().size());
+		assertTrue(monsterMaxHealth < playerMonster.getMaxHealth());
+		assertTrue(monsterArmorAmount < playerMonster.getArmorAmount());
+				
+		monsterArmorAmount = playerMonster.getArmorAmount();
+		monsterMaxHealth = playerMonster.getMaxHealth();
+		
+		//SpeedPotion test
+		testInventory.useItem(testInventory.getItems().get(0), playerMonster);
+		assertEquals(2, testInventory.getItems().size());
+		assertFalse(monsterSpeed == playerMonster.getSpeed());
+		
+		monsterSpeed = playerMonster.getSpeed();
+		
+		//Weapon test
+		testInventory.useItem(testInventory.getItems().get(0), playerMonster);
+		assertEquals(1, testInventory.getItems().size());
+		assertTrue(monsterAttackDamage < playerMonster.getAttackAmount());
+		
+		monsterAttackDamage = playerMonster.getAttackAmount();
+		
+		
 	}
 }
