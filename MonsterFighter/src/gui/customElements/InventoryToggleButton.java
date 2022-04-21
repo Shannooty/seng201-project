1cc295a5529c731049b8aa2b9e1ce62f77a31136
Shannooty <75523274+Shannooty@users.noreply.github.com>
2022-04-22 03:10:45 +1200

    InventoryToggleButton.java javadoc

--- MonsterFighter/src/gui/customElements/InventoryToggleButton.java
+++ MonsterFighter/src/gui/customElements/InventoryToggleButton.java
@@ -6,19 +6,23 @@ import javax.swing.JToggleButton;
 
 import day.Battle;
 import interfaces.HasImage;
-import purchasable.Purchasable;
-import purchasable.items.Item;
-import purchasable.monsters.Monster;
 
+/**
+ * Custom toggle button for storing a object reference to be used in the ImgInventoryPanel. Objects must extend HasImage interface.
+ * @author Bede Nathan, Celia Allen
+ *
+ */
 public class InventoryToggleButton extends JToggleButton {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -5449563501811900529L;
 	
 	private HasImage buttonObject;
 	
+	/**
+	 * Constructor for the toggle button.
+	 * @param <T> Class object which extends HasImage interface
+	 * @param item A object which extends HasImage interface
+	 */
 	public <T extends HasImage> InventoryToggleButton(T item) {
 		setButtonObject(item);
 		this.setIcon(item.getImg());
@@ -26,20 +30,32 @@ public class InventoryToggleButton extends JToggleButton {
 		this.setSelectedIcon(new ImageIcon(InventoryToggleButton.class.getResource("/images/index1.png")));
 		
 		if (item instanceof Battle) {
-			setBattleInfo((Battle)item);
+			setBattleInfo();
 		}
 	}
-
+	
+	/**
+	 * Getter for the buttonObject
+	 * @return The object stored in the button
+	 */
 	public HasImage getButtonObject() {
 		return buttonObject;
 	}
 	
-	public void setBattleInfo(Battle battle) {
+	/**
+	 * Sets info for the battle if the button object is type Battle
+	 */
+	public void setBattleInfo() {
+		Battle battle = (Battle) getButtonObject();
 		this.setText("Number of monsters to fight: "+Integer.toString(battle.getNumMonsters()));
 		this.setHorizontalTextPosition(JLabel.CENTER);
 		this.setVerticalTextPosition(JLabel.CENTER);
 	}
 	
+	/**
+	 * Sets the buttons stored object.
+	 * @param item Object to be stored
+	 */
 	private void setButtonObject(HasImage item) {
 		this.buttonObject = item;
 	}
