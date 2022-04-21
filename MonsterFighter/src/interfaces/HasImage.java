63ef139e901881ebad7b079d20c0e5f917933955
Shannooty <75523274+Shannooty@users.noreply.github.com>
2022-04-22 02:07:30 +1200

    Create HasImage.java interface

---
+++ MonsterFighter/src/interfaces/HasImage.java
@@ -0,0 +1,11 @@
+package interfaces;
+
+import javax.swing.ImageIcon;
+
+public interface HasImage {
+	
+	public ImageIcon getImg();
+	public void setImg();
+	public void setImgPath(String imgPath);
+	
+}
