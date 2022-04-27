5ca27659f9da0dc8a898c953df240a70e0343dd4
Chamille9 <cal135@uclive.ac.nz>
2022-04-27 14:26:21 +1200

    ItemTest created??? Not fully sure what is required here

--- MonsterFighter/src/test/ItemTest.java
+++ MonsterFighter/src/test/ItemTest.java
@@ -9,122 +9,60 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import purchasable.items.Item;
+import purchasable.items.armors.*;
+import purchasable.items.food.*;
+import purchasable.items.weapons.*;
+import purchasable.monsters.Monster;
 
 class ItemTest {
 
-	private Item testItem;
+	private Monster testMonster;
+	private Weapon testWeapon;
+	private Food testFood;
+	private Armor testArmor;
+
 
 	@BeforeEach
 	void setUp() throws Exception {
+		testWeapon = new Knife();
+		testFood = new Apple();
+		testArmor = new Shield();
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		testWeapon = null;
+		testFood = null;
+		testArmor = null;
 	}
 
-	@Test
-	void testBuy() {
-		fail("Not yet implemented");
-	}
 
-	@Test
-	void testSell() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testItem() {
-		fail("Not yet implemented");
-	}
 
 	@Test
 	void testUse() {
-		fail("Not yet implemented");
+		int monsterOldDamage = testMonster.getAttackAmount();
+		testWeapon.use(testMonster);
+		assertTrue(testMonster.getAttackAmount() == monsterOldDamage + testWeapon.getDamage());
+		
+		int monsterOldHealth = testMonster.getHealth();
+		testFood.use(testMonster);
+		assertTrue(testMonster.getHealth() == monsterOldHealth + testFood.getHealAmount());
+		
+		int monsterOldMaxHealth = testMonster.getMaxHealth();
+		int monsterOldArmorAmount = testMonster.getArmorAmount();
+		testArmor.use(testMonster);
+		assertTrue(testMonster.getMaxHealth() == monsterOldMaxHealth + testArmor.getHealthIncrease());
+		assertTrue(testMonster.getArmorAmount() == monsterOldArmorAmount + testArmor.getArmorIncrease());
 	}
 
-	@Test
-	void testCreateDescription() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testGetSellBackDescription() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testGetBuyDescription() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testPurchasable() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testSetName() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testGetName() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testGetPurchasePrice() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testGetSellPrice() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testGetDiscription() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testSetDiscription() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testSetPurchasePrice() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testSetSellPrice() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testGetImg() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testSetImg() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testSetImgPath() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testGetID() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testSetInitialID() {
-		fail("Not yet implemented");
-	}
+//	@Test
+//	void testCreateDescription() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	void testGetSellBackDescription() {
+//		fail("Not yet implemented");
+//	}
 
 }
