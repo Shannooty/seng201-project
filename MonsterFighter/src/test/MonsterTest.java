d5d5baffcb302fdb2fb8d0ff727b61767908cc28
Shannooty <75523274+Shannooty@users.noreply.github.com>
2022-04-27 16:44:06 +1200

    Wrote all MonsterTest tests, a lot of bugs to fix

--- MonsterFighter/src/test/MonsterTest.java
+++ MonsterFighter/src/test/MonsterTest.java
@@ -8,24 +8,34 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import player.Player;
+import player.Team;
 import purchasable.items.armors.*;
+import purchasable.items.weapons.*;
 import purchasable.monsters.*;
 
 class MonsterTest {
 	
 	private Monster testMonster;
 	private Armor testArmor;
+	private Weapon testWeapon;
+	private Player testPlayer;
 	
 	@BeforeEach
 	void setUp() throws Exception {
 		testMonster = new Slime();
 		testArmor = new Chainmail();
+		testWeapon = new SledgeHammer();
+		testPlayer = new Player("Tom", new Skeleton());
+		testPlayer.addGold(10000);
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
 		testMonster = null;
 		testArmor = null;
+		testWeapon = null;
+		testPlayer = null;
 	}
 
 	@Test
@@ -206,5 +216,175 @@ class MonsterTest {
 			}
 			currentSpeed = testMonster.getSpeed();
 		}
+	}
+	
+	@Test
+	void testAddAttackDamage() {
+		ArrayList<Integer> attackValues = new ArrayList<Integer>();
+		attackValues.add(10);
+		attackValues.add(15);
+		attackValues.add(50);
+		attackValues.add(0);
+		attackValues.add(-10);
+		attackValues.add(Integer.MAX_VALUE);
+		attackValues.add(Integer.MIN_VALUE);
+		
+		int currentAttackDamage = testMonster.getAttackAmount();
+		
+		for (int attackIncrease : attackValues) {
+			testMonster.addAttackAmount(attackIncrease);
+			
+			assertTrue(testMonster.getAttackAmount() >= 0);
+			assertTrue(testMonster.getAttackAmount() >= currentAttackDamage);
+			
+			if (attackIncrease <= 0) {
+				assertEquals(testMonster.getAttackAmount(), currentAttackDamage);
+			}
+			currentAttackDamage = testMonster.getAttackAmount();
+		}
+	}
+	
+	@Test
+	void testRemoveAttackDamage() {
+		ArrayList<Integer> attackValues = new ArrayList<Integer>();
+		attackValues.add(10);
+		attackValues.add(15);
+		attackValues.add(50);
+		attackValues.add(0);
+		attackValues.add(-10);
+		attackValues.add(Integer.MAX_VALUE);
+		attackValues.add(Integer.MIN_VALUE);
+		
+		int currentAttackDamage = testMonster.getAttackAmount();
+		
+		for (int attackDecrease : attackValues) {
+			testMonster.removeAttackAmount(attackDecrease);
+			
+			assertTrue(testMonster.getAttackAmount() >= 0);
+			assertTrue(testMonster.getAttackAmount() <= currentAttackDamage);
+			
+			if (attackDecrease <= 0) {
+				assertEquals(testMonster.getAttackAmount(), currentAttackDamage);
+			}
+			currentAttackDamage = testMonster.getAttackAmount();
+		}
+	}
+	
+	@Test
+	void testSleep() {
+		testMonster.removeHealth(500);
+		int expectedHealth = testMonster.getHealth() + testMonster.getHealAmount();
+		
+		testMonster.sleep();
+		assertEquals(expectedHealth, testMonster.getHealth());
+		
+		//Test sleeping reseting stunned status
+		testMonster.removeHealth(100000);
+		assertTrue(testMonster.getStunnedStatus());
+		
+		testMonster.sleep();
+		assertFalse(testMonster.getStunnedStatus());
+	}
+	
+	@Test
+	void testAddWeapon() {
+		int oldMonsterAttack = testMonster.getAttackAmount();
+		int expectedAttack = oldMonsterAttack + testWeapon.getDamage();
+		
+		testMonster.addWeapon(testWeapon);
+		assertEquals(expectedAttack, testMonster.getAttackAmount());
+		
+		Weapon stick = new SharpStick();
+		expectedAttack = oldMonsterAttack + stick.getDamage();
+		
+		Weapon oldWeapon = testMonster.addWeapon(stick);
+		assertEquals(expectedAttack, testMonster.getAttackAmount());
+		assertEquals(testWeapon, oldWeapon);
+	}
+	
+	@Test
+	void testRemoveWeapon() {
+		int baseAttackDamage = testMonster.getAttackAmount();
+		testMonster.addWeapon(testWeapon);
+		
+		Weapon oldWeapon = testMonster.removeWeapon();
+		assertEquals(baseAttackDamage, testMonster.getAttackAmount());
+		assertEquals(oldWeapon, testWeapon);
+		
+		//Test removing weapon from monster with no weapon
+		oldWeapon = testMonster.removeWeapon();
+		assertEquals(baseAttackDamage, testMonster.getAttackAmount());
+		assertEquals(oldWeapon, null);
+	}
+	
+	@Test
+	void testAddArmor() {
+		int oldMonsterArmor = testMonster.getArmorAmount();
+		int oldMonsterMaxHealth = testMonster.getMaxHealth();
+		int oldMonsterHealth = testMonster.getHealth();
+		
+		int expectedArmor = oldMonsterArmor + testArmor.getArmorIncrease();
+		int expectedMaxHealth = oldMonsterMaxHealth + testArmor.getHealthIncrease();
+		int expectedHealth = oldMonsterHealth + testArmor.getHealthIncrease();
+		
+		Armor oldArmor = testMonster.addArmor(testArmor);
+		assertEquals(expectedArmor, testMonster.getArmorAmount());
+		assertEquals(expectedMaxHealth, testMonster.getMaxHealth());
+		assertEquals(expectedHealth, testMonster.getHealth());
+		assertEquals(oldArmor, null);
+		
+		FlowerCrown crown = new FlowerCrown();
+		expectedArmor = oldMonsterArmor + crown.getArmorIncrease();
+		expectedMaxHealth = oldMonsterMaxHealth + crown.getHealthIncrease();
+		expectedHealth = oldMonsterHealth + crown.getHealthIncrease();
+		
+		oldArmor = testMonster.addArmor(testArmor);
+		assertEquals(expectedArmor, testMonster.getArmorAmount());
+		assertEquals(expectedMaxHealth, testMonster.getMaxHealth());
+		assertEquals(expectedHealth, testMonster.getHealth());
+		assertEquals(oldArmor, testArmor);
+	}
+	
+	@Test
+	void testRemoveArmor() {
+		int baseMonsterArmor = testMonster.getArmorAmount();
+		int baseMonsterMaxHealth = testMonster.getMaxHealth();
+		int baseMonsterHealth = testMonster.getHealth();
+		testMonster.addArmor(testArmor);
+		
+		Armor oldArmor = testMonster.removeArmor();
+		assertEquals(baseMonsterArmor, testMonster.getArmorAmount());
+		assertEquals(baseMonsterMaxHealth, testMonster.getMaxHealth());
+		assertEquals(baseMonsterHealth, testMonster.getHealth());
+		assertEquals(oldArmor, testArmor);
+		
+		//Test removing armor from monster who has no armor
+		oldArmor = testMonster.removeArmor();
+		assertEquals(baseMonsterArmor, testMonster.getArmorAmount());
+		assertEquals(baseMonsterMaxHealth, testMonster.getMaxHealth());
+		assertEquals(baseMonsterHealth, testMonster.getHealth());
+		assertEquals(oldArmor, null);
+	}
+	
+	@Test
+	void testBuy() {
+		Team playerTeam = testPlayer.getInventory().getTeam();
+		double expectedGold = testPlayer.getGoldAmount() - testMonster.getPurchasePrice();
+		assertFalse(playerTeam.getTeam().contains(testMonster));
+		
+		testMonster.buy(testPlayer);
+		assertTrue(playerTeam.getTeam().contains(testMonster));
+		assertEquals(expectedGold, testPlayer.getGoldAmount());
+	}
+	
+	@Test
+	void testSell() {
+		Team playerTeam = testPlayer.getInventory().getTeam();
+		testMonster.buy(testPlayer);
+		double expectedGold = testPlayer.getGoldAmount() + testMonster.getPurchasePrice();
+		
+		testMonster.sell(testPlayer);
+		assertFalse(playerTeam.getTeam().contains(testMonster));
+		assertEquals(expectedGold, testPlayer.getGoldAmount());
 	}
 }
