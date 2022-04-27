b25da7f0373d10eeec5989d573ba7717d588c242
Chamille9 <cal135@uclive.ac.nz>
2022-04-27 13:02:19 +1200

    Initialised ItemTest

---
+++ MonsterFighter/src/test/ItemTest.java
@@ -0,0 +1,61 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import purchasable.items.Item;
+
+class ItemTest {
+	
+	private Item testItem;
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		testItem = null;
+	}
+
+	@Test
+	void testBuy() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSell() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testItem() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUse() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCreateDescription() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetSellBackDescription() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetBuyDescription() {
+		fail("Not yet implemented");
+	}
+
+}
