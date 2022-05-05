2db66617f137512a25c7d854ebc7d239527e82ca
Chamille9 <cal135@uclive.ac.nz>
2022-05-05 14:44:07 +1200

    Javadoc for TeamFullException

--- MonsterFighter/src/exceptions/TeamFullException.java
+++ MonsterFighter/src/exceptions/TeamFullException.java
@@ -1,14 +1,26 @@
 package exceptions;
 
+/**
+ * 
+ * @author 
+ *
+ */
 public class TeamFullException extends IllegalArgumentException {
 
 	/**
-	 * 
+	 * Attribute serialVersionUID, of type static final long. A serialVersion.
 	 */
 	private static final long serialVersionUID = 5315714111120614979L;
 	
+	/**
+	 * Constructor for TeamFullException. Accepts no parameters. Returns nothing.
+	 */
 	public TeamFullException() {};
 	
+	/**
+	 * Constructor for TeamFullException. Calls the SuperClass constructor for IllegalArgumentException.
+	 * @param message, of type String. The message to pass to the SuperClass constructor.
+	 */
 	public TeamFullException(String message) {
 		super(message);
 	}
