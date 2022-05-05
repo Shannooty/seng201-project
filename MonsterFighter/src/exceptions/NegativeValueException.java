5bc8e1204996b16fa84789f64353ea4a0967b209
Chamille9 <cal135@uclive.ac.nz>
2022-05-05 14:43:26 +1200

    Javadoc for NegativeValueException

--- MonsterFighter/src/exceptions/NegativeValueException.java
+++ MonsterFighter/src/exceptions/NegativeValueException.java
@@ -1,14 +1,26 @@
 package exceptions;
 
+/**
+ * 
+ * @author
+ *
+ */
 public class NegativeValueException extends IllegalArgumentException {
 
 	/**
-	 * 
+	 * Attribute serialVersionUID, of type static final long. A serialVersion.
 	 */
 	private static final long serialVersionUID = -4596000100030514801L;
 	
+	/**
+	 * Constructor for NegativeValueException. Accepts no parameters. Returns nothing.
+	 */
 	public NegativeValueException() {}
 	
+	/**
+	 * Constructor for NegativeValueException. Calls the SuperClass constructor for IllegalArgumentException.
+	 * @param message, of type String. The message to pass to the SuperClass constructor.
+	 */
 	public NegativeValueException(String message) {
 		super(message);
 	}
