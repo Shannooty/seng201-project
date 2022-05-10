12e645dee8dd7f6c0f56163a9cfac9320cb01d14
Shannooty <75523274+Shannooty@users.noreply.github.com>
2022-05-10 15:54:43 +1200

    Update WeaponTest.java

--- MonsterFighter/src/test/WeaponTest.java
+++ MonsterFighter/src/test/WeaponTest.java
@@ -2,43 +2,46 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import purchasable.items.weapons.*;
+import purchasable.monsters.*;
 
 class WeaponTest {
 //	NOTE: NOT SURE IF THIS TEST IS NECESSARY
 
 	private Weapon testWeapon;
+	private Weapon testWeapon2;
+	private Monster testMonster;
 
 
 	@BeforeEach
 	void setUp() throws Exception {
 		testWeapon = new Knife();
+		testWeapon2 = new Knife();
+		testMonster = new Snake();
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
 		testWeapon = null;
-	}
-
-	@Test
-	void testWeapon() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testGetDamage() {
-		fail("Not yet implemented");
+		testWeapon2 = null;
 	}
 
 	@Test
 	void testUseMonster() {
-		fail("Not yet implemented");
+		int oldMonsterDamage = testMonster.getAttackAmount();
+		
+		Weapon oldWeapon = testWeapon.use(testMonster);
+		assertEquals(oldMonsterDamage + testWeapon.getDamage(), testMonster.getAttackAmount());
+		assertEquals(null, oldWeapon);
+		
+		oldMonsterDamage = testMonster.getAttackAmount();
+		oldWeapon = testWeapon2.use(testMonster);
+		assertEquals(oldMonsterDamage, testMonster.getAttackAmount());
+		assertEquals(testWeapon, oldWeapon);
 	}
 
 }
