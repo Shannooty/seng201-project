ee98c9a04cde6488be733865116db422e5105764
Shannooty <75523274+Shannooty@users.noreply.github.com>
2022-05-12 16:24:44 +1200

    Purchasable registries javadoc

--- MonsterFighter/src/generators/registries/AvalibleArmors.java
+++ MonsterFighter/src/generators/registries/AvalibleArmors.java
@@ -2,11 +2,21 @@ package generators.registries;
 
 import java.util.Random;
 
+/**
+ * Public enum of all Armor types
+ * @author Bede Nathan
+ * @author Celia Allen
+ *
+ */
 public enum AvalibleArmors {
 	FLOWERCROWN, SHIELD, CHAINMAIL;
 	
 	private static final AvalibleArmors[] VALUES = AvalibleArmors.values();
 	
+	/**
+	 * Static method that returns a random Armor type from the AvalibleArmor enum
+	 * @return Armor type
+	 */
 	public static AvalibleArmors randomArmor() {
 		return VALUES[new Random().nextInt(VALUES.length)];
 	}
--- MonsterFighter/src/generators/registries/AvalibleWeapons.java
+++ MonsterFighter/src/generators/registries/AvalibleWeapons.java
@@ -2,11 +2,21 @@ package generators.registries;
 
 import java.util.Random;
 
+/**
+ * Public enum of all Weapon types
+ * @author Bede Nathan
+ * @author Celia Allen
+ *
+ */
 public enum AvalibleWeapons {
 	SHARPSTICK, SLEDGEHAMMER, KNIFE;
 	
-private static final AvalibleWeapons[] VALUES = AvalibleWeapons.values();
+	private static final AvalibleWeapons[] VALUES = AvalibleWeapons.values();
 	
+	/**
+	 * Static method that returns a random Weapon type from the AvalibleWeapons enum
+	 * @return Weapon type
+	 */
 	public static AvalibleWeapons randomWeapon() {
 		return VALUES[new Random().nextInt(VALUES.length)];
 	}
