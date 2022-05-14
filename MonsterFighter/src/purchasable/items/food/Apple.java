a279ccec45b40e41d25d70207887d98fd1efd6e1
Chamille9 <cal135@uclive.ac.nz>
2022-05-14 13:47:32 +1200

    Javadoc for Food subclasses

--- MonsterFighter/src/purchasable/items/food/Apple.java
+++ MonsterFighter/src/purchasable/items/food/Apple.java
@@ -1,7 +1,17 @@
 package purchasable.items.food;
 
+/**
+ * Extends Food. A type of item that the player can buy in the shop.
+ * @author Bede Nathan
+ * @author Celia Allen
+ *
+ */
 public class Apple extends Food {
-	
+
+	/**
+	 * Constructor for Apple. Calls the superclass constructor with the name of the Apple.
+	 *  Sets the purchase price, sell-back price, image path and image for the Apple.
+	 */
 	public Apple() {
 		super("Apple");
 		setHealAmount(100);
--- MonsterFighter/src/purchasable/items/food/Bread.java
+++ MonsterFighter/src/purchasable/items/food/Bread.java
@@ -1,7 +1,17 @@
 package purchasable.items.food;
 
+/**
+ * Extends Food. A type of item that the player can buy in the shop.
+ * @author Celia Allen
+ * @author Bede Nathan
+ *
+ */
 public class Bread extends Food {
 	
+	/**
+	 * Constructor for Bread. Calls the superclass constructor with the name of the Bread.
+	 *  Sets the purchase price, sell-back price, image path and image for the Bread.
+	 */
 	public Bread() {
 		super("Bread");
 		setHealAmount(120);
