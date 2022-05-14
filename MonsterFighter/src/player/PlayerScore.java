a3275d6e59e79fa3975496b54eb0531d55ad82d4
Chamille9 <cal135@uclive.ac.nz>
2022-05-14 13:11:12 +1200

    Edited javadoc in PlayerScore

--- MonsterFighter/src/player/PlayerScore.java
+++ MonsterFighter/src/player/PlayerScore.java
@@ -4,17 +4,25 @@ import exceptions.NegativeValueException;
 
 /**
  * The PlayerScore class deals with storing the points scored by the player in a way that can be used by a leaderboard
- * @author Bede Nathan, Celia Allen
+ * @author Bede Nathan
+ * @author Celia Allen
  *
  */
 public class PlayerScore implements Comparable<PlayerScore>{
 	
+	/**
+	 * Attribute playerName of type String. The player's name.
+	 */
 	private String playerName;
+	
+	/**
+	 * Attribute points of type integer. The amount of points the player has.
+	 */
 	private int points = 0;
 	
 	/**
 	 * Constructor to be used by the player class
-	 * @param player the player whose score will be stored
+	 * @param player, of type Player. The player whose score will be stored
 	 */
 	public PlayerScore(Player player) {
 		setName(player.getName());
@@ -22,8 +30,8 @@ public class PlayerScore implements Comparable<PlayerScore>{
 	
 	/**
 	 * Constructor for the PlayerScore class used for previously saved scores
-	 * @param playerName name of the player
-	 * @param playerScore string representation of the score
+	 * @param playerName, of type String. Name of the player
+	 * @param playerScore, of type String. String representation of the score
 	 */
 	public PlayerScore(String playerName, String playerScore) {
 		setName(playerName);
@@ -31,8 +39,8 @@ public class PlayerScore implements Comparable<PlayerScore>{
 	}
 	
 	/**
-	 * Increases the number of points the player has
-	 * @param numPoints number of points to increase by
+	 * Increases the number of points the player has.
+	 * @param numPoints, of type integer. Number of points to increase by
 	 */
 	public void addPoints(int numPoints) throws NegativeValueException {
 		if (numPoints < 0) {
@@ -48,7 +56,7 @@ public class PlayerScore implements Comparable<PlayerScore>{
 	
 	/**
 	 * Returns the points the player currently has
-	 * @return the number of points
+	 * @return points, of type integer. The number of points
 	 */
 	public int getPoints() {
 		return points;
@@ -56,7 +64,7 @@ public class PlayerScore implements Comparable<PlayerScore>{
 	
 	/**
 	 * Returns the name of the player
-	 * @return the player name
+	 * @return playerName, of type String. The player name
 	 */
 	public String getName() {
 		return playerName;
@@ -64,7 +72,7 @@ public class PlayerScore implements Comparable<PlayerScore>{
 	
 	/**
 	 * Sets the player name for the class
-	 * @param name the name of the player
+	 * @param name, of type String. The name of the player
 	 */
 	private void setName(String name) {
 		playerName = name;
@@ -72,7 +80,7 @@ public class PlayerScore implements Comparable<PlayerScore>{
 	
 	/**
 	 * Provides a natural ordering to different PlayerScores so they can be sorted.
-	 * @return int value to determine order
+	 * @return compareValue, of type integer. The value to determine order
 	 */
 	public int compareTo(PlayerScore other) {
 		Integer thisScore = this.getPoints();
