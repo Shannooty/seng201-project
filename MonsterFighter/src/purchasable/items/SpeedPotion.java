6cfead0454060c2f03b04b074fdc73c2767c7a6d
Chamille9 <cal135@uclive.ac.nz>
2022-05-14 13:33:42 +1200

    Javadoc for SpeedPotion

--- MonsterFighter/src/purchasable/items/SpeedPotion.java
+++ MonsterFighter/src/purchasable/items/SpeedPotion.java
@@ -2,10 +2,21 @@ package purchasable.items;
 
 import purchasable.monsters.Monster;
 
+/**
+ * Extends Item. An item that the player can buy in the Shop.
+ * @author inkic
+ *
+ */
 public class SpeedPotion extends Item {
 	
+	/**
+	 * Attribute speedIncrease of type integer. The amount by which the Monster's speed increases.
+	 */
 	private int speedIncrease = 10;
 	
+	/**
+	 * Constructor for SpeedPotion. Calls the superclass constructor with the name of the SpeedPotion, then sets the purchase price, sell-back price, image path and image for the SpeedPotion.
+	 */
 	public SpeedPotion() {
 		super("Speed Potion");
 		setPurchasePrice(13);
@@ -14,10 +25,17 @@ public class SpeedPotion extends Item {
 		setImg();
 	}
 	
+	/**
+	 * Returns the speed increase of the SpeedPotion.
+	 * @return speedIncrease, of type integer.
+	 */
 	public int getSpeedIncrease() {
 		return speedIncrease;
 	}
 	
+	/**
+	 * Lets the player use the SpeedPotion on a Monster. Adds the speed increase to the Monster's speed.
+	 */
 	@Override
 	public Item use(Monster monster) {
 		monster.addSpeed(getSpeedIncrease());
