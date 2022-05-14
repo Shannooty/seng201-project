9d726b4338236cc4e992e8025d907259a941d369
Chamille9 <cal135@uclive.ac.nz>
2022-05-14 14:27:10 +1200

    Javadoc for Monster subclasses

--- MonsterFighter/src/purchasable/monsters/Dinosaur.java
+++ MonsterFighter/src/purchasable/monsters/Dinosaur.java
@@ -1,7 +1,17 @@
 package purchasable.monsters;
 
+/**
+ * Extends Monster. A type of monster that the player can own or buy in the shop.
+ * @author Celia Allen
+ * @author Bede Nathan
+ *
+ */
 public class Dinosaur extends Monster {
 	
+	/**
+	 * Constructor for Dinosaur. Calls the superclass constructor with the name of the Dinosaur, its maximum health, attack amount, and speed.
+	 *  Sets the image path, image, heal amount, type purchase price and sell back price for the Dinosaur.
+	 */
 	public Dinosaur() {
 		super("Rexter", 800, 400, 12);
 		setImgPath("/images/dinosaur.png");
--- MonsterFighter/src/purchasable/monsters/Snake.java
+++ MonsterFighter/src/purchasable/monsters/Snake.java
@@ -1,7 +1,17 @@
 package purchasable.monsters;
 
+/**
+ * Extends Monster. A type of monster that the player can own or buy in the shop.
+ * @author Celia Allen
+ * @author Bede Nathan
+ *
+ */
 public class Snake extends Monster {
 	
+	/**
+	 * Constructor for Snake. Calls the superclass constructor with the name of the Snake, its maximum health, attack amount, and speed.
+	 *  Sets the image path, image, heal amount, type purchase price and sell back price for the Snake.
+	 */
 	public Snake() {
 		super("Snek", 700, 320, 18);
 		setImgPath("/images/snake.png");
