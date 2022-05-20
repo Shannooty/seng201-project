e80aa60d68f04e613602add9974baefe2fc1cce6
nrb55 <nrb55@cs18223kq.canterbury.ac.nz>
2022-05-21 10:02:19 +1200

    Heal amount string

--- MonsterFighter/src/purchasable/monsters/Monster.java
+++ MonsterFighter/src/purchasable/monsters/Monster.java
@@ -454,7 +454,7 @@ public abstract class Monster extends Purchasable implements Comparator<Monster>
 				equippedString += equipped.get(i);
 			}
 		}
-		return "Type: "+type+"\nName: "+getName()+"\n\nHealth: "+ getHealth() + " / " + getMaxHealth() +"\nStatus: "+stunnedStatus+"\n\nAttack Amount: "+getAttackAmount()+"\nArmor Amount: "+getArmorAmount()+"\nEquipped: "+equippedString+"\nSpeed: "+getSpeed()+"\n\nSell-back Price: "+getPurchasePrice();
+		return "Type: "+type+"\nName: "+getName()+"\n\nHealth: "+ getHealth() + " / " + getMaxHealth() +"\nHeal Amount: "+ getHealAmount() + "\nStatus: "+stunnedStatus+"\n\nAttack Amount: "+getAttackAmount()+"\nArmor Amount: "+getArmorAmount()+"\nEquipped: "+equippedString+"\nSpeed: "+getSpeed()+"\n\nSell-back Price: "+getPurchasePrice();
 	}
 	
 	/**
